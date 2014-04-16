aa3ba0c412fd7cddc8b30708e8b7ae4357e57e44
noise0607 <noise0607@gmail.com>
2014-04-16 16:17:22 +0900

    test

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-
-phpinfo();
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Ryota-ka.me</title>
+	</head>
+	<body>
+		<h1>Ryota-ka.me</h1>
+		<p><?php echo date('yy.mm.dd H:i:s') ?></p>
+	</body>
+</html>
