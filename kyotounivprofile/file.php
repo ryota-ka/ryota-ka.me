c8a3cae1836df0a21c57e00845fd537216035063
noise0607 <noise0607@gmail.com>
2014-04-16 23:31:34 +0900

    modified mobileconfig generator

--- kyotounivprofile/file.php
+++ kyotounivprofile/file.php
@@ -4,182 +4,157 @@ $ecsid = 'a0127689';
 $name = '亀岡 亮太';
 $localpart = 'kameoka.ryota.53x';
 
-$file = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL .
-		'<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' . PHP_EOL .
-		'<plist version="1.0">' . PHP_EOL .
-		'<dict>' . PHP_EOL .
-		'	<key>PayloadContent</key>' . PHP_EOL .
-		'	<array>' . PHP_EOL .
-		'		<dict>' . PHP_EOL .
-		'			<key>FullScreen</key>' . PHP_EOL .
-		'			<false/>' . PHP_EOL .
-		'			<key>IsRemovable</key>' . PHP_EOL .
-		'			<true/>' . PHP_EOL .
-		'			<key>Label</key>' . PHP_EOL .
-		'			<string>Common Portal for All Students</string>' . PHP_EOL .
-		'			<key>PayloadDescription</key>' . PHP_EOL .
-		'			<string>Web クリップを構成します</string>' . PHP_EOL .
-		'			<key>PayloadDisplayName</key>' . PHP_EOL .
-		'			<string>Web クリップ（Common Portal for All Students）</string>' . PHP_EOL .
-		'			<key>PayloadIdentifier</key>' . PHP_EOL .
-		'			<string>com.ryotakameoka.kyotounivprofile.WebClip</string>' . PHP_EOL .
-		'			<key>PayloadOrganization</key>' . PHP_EOL .
-		'			<string>Ryota Kameoka</string>' . PHP_EOL .
-		'			<key>PayloadType</key>' . PHP_EOL .
-		'			<string>com.apple.webClip.managed</string>' . PHP_EOL .
-		'			<key>PayloadUUID</key>' . PHP_EOL .
-		'			<string>73784257-7574-4D78-BD73-7396B77950EC</string>' . PHP_EOL .
-		'			<key>PayloadVersion</key>' . PHP_EOL .
-		'			<integer>1</integer>' . PHP_EOL .
-		'			<key>Precomposed</key>' . PHP_EOL .
-		'			<false/>' . PHP_EOL .
-		'			<key>URL</key>' . PHP_EOL .
-		'			<string>https://cert.iimc.kyoto-u.ac.jp/fw/dfw?AGENT_DFW=http%3a%2f%2fstudent.iimc.kyoto-u.ac.jp%2f&amp;path=%2fiwproxy%2fDMP%2fdp%2fdmp&amp;query=</string>' . PHP_EOL .
-		'		</dict>' . PHP_EOL .
-		'		<dict>' . PHP_EOL .
-		'			<key>AutoJoin</key>' . PHP_EOL .
-		'			<true/>' . PHP_EOL .
-		'			<key>HIDDEN_NETWORK</key>' . PHP_EOL .
-		'			<false/>' . PHP_EOL .
-		'			<key>PayloadDescription</key>' . PHP_EOL .
-		'			<string>ワイヤレス接続設定を構成します。</string>' . PHP_EOL .
-		'			<key>PayloadDisplayName</key>' . PHP_EOL .
-		'			<string>Wi-Fi（MIAKO）</string>' . PHP_EOL .
-		'			<key>PayloadIdentifier</key>' . PHP_EOL .
-		'			<string>com.ryotakameoka.kyotounivprofile.</string>' . PHP_EOL .
-		'			<key>PayloadOrganization</key>' . PHP_EOL .
-		'			<string>Ryota Kameoka</string>' . PHP_EOL .
-		'			<key>PayloadType</key>' . PHP_EOL .
-		'			<string>com.apple.wifi.managed</string>' . PHP_EOL .
-		'			<key>PayloadUUID</key>' . PHP_EOL .
-		'			<string>81078CC2-BC4E-4D41-8EC7-D3028D34C000</string>' . PHP_EOL .
-		'			<key>PayloadVersion</key>' . PHP_EOL .
-		'			<integer>1</integer>' . PHP_EOL .
-		'			<key>ProxyType</key>' . PHP_EOL .
-		'			<string>None</string>' . PHP_EOL .
-		'			<key>SSID_STR</key>' . PHP_EOL .
-		'			<string>MIAKO</string>' . PHP_EOL .
-		'		</dict>' . PHP_EOL .
-		'		<dict>' . PHP_EOL .
-		'			<key>EmailAccountDescription</key>' . PHP_EOL .
-		'			<string>KUMOI</string>' . PHP_EOL .
-		'			<key>EmailAccountName</key>' . PHP_EOL .
-		'			<string>' . $name . '/string>' . PHP_EOL .
-		'			<key>EmailAccountType</key>' . PHP_EOL .
-		'			<string>EmailTypeIMAP</string>' . PHP_EOL .
-		'			<key>EmailAddress</key>' . PHP_EOL .
-		'			<string>' . $localpart . '@st.kyoto-u.ac.jp</string>' . PHP_EOL .
-		'			<key>IncomingMailServerAuthentication</key>' . PHP_EOL .
-		'			<string>EmailAuthPassword</string>' . PHP_EOL .
-		'			<key>IncomingMailServerHostName</key>' . PHP_EOL .
-		'			<string>outlook.office365.com</string>' . PHP_EOL .
-		'			<key>IncomingMailServerPortNumber</key>' . PHP_EOL .
-		'			<integer>993</integer>' . PHP_EOL .
-		'			<key>IncomingMailServerUseSSL</key>' . PHP_EOL .
-		'			<true/>' . PHP_EOL .
-		'			<key>IncomingMailServerUsername</key>' . PHP_EOL .
-		'			<string>' . $ecsid . '@st.kyoto-u.ac.jp</string>' . PHP_EOL .
-		'			<key>OutgoingMailServerAuthentication</key>' . PHP_EOL .
-		'			<string>EmailAuthPassword</string>' . PHP_EOL .
-		'			<key>OutgoingMailServerHostName</key>' . PHP_EOL .
-		'			<string>smtp.office365.com</string>' . PHP_EOL .
-		'			<key>OutgoingMailServerPortNumber</key>' . PHP_EOL .
-		'			<integer>587</integer>' . PHP_EOL .
-		'			<key>OutgoingMailServerUseSSL</key>' . PHP_EOL .
-		'			<true/>' . PHP_EOL .
-		'			<key>OutgoingMailServerUsername</key>' . PHP_EOL .
-		'			<string>' . $ecsid . '@st.kyoto-u.ac.jp</string>' . PHP_EOL .
-		'			<key>OutgoingPasswordSameAsIncomingPassword</key>' . PHP_EOL .
-		'			<true/>' . PHP_EOL .
-		'			<key>PayloadDescription</key>' . PHP_EOL .
-		'			<string>メールアカウントを構成します。</string>' . PHP_EOL .
-		'			<key>PayloadDisplayName</key>' . PHP_EOL .
-		'			<string>IMAP アカウント（KUMOI）</string>' . PHP_EOL .
-		'			<key>PayloadIdentifier</key>' . PHP_EOL .
-		'			<string>com.ryotakameoka.kyotounivprofile.メール</string>' . PHP_EOL .
-		'			<key>PayloadOrganization</key>' . PHP_EOL .
-		'			<string>Ryota Kameoka</string>' . PHP_EOL .
-		'			<key>PayloadType</key>' . PHP_EOL .
-		'			<string>com.apple.mail.managed</string>' . PHP_EOL .
-		'			<key>PayloadUUID</key>' . PHP_EOL .
-		'			<string>C7998766-BDE1-4ACA-96B7-67D9FB750F2D</string>' . PHP_EOL .
-		'			<key>PayloadVersion</key>' . PHP_EOL .
-		'			<integer>1</integer>' . PHP_EOL .
-		'			<key>PreventAppSheet</key>' . PHP_EOL .
-		'			<false/>' . PHP_EOL .
-		'			<key>PreventMove</key>' . PHP_EOL .
-		'			<false/>' . PHP_EOL .
-		'			<key>SMIMEEnabled</key>' . PHP_EOL .
-		'			<false/>' . PHP_EOL .
-		'		</dict>' . PHP_EOL .
-		'		<dict>' . PHP_EOL .
-		'			<key>EAP</key>' . PHP_EOL .
-		'			<dict/>' . PHP_EOL .
-		'			<key>IPv4</key>' . PHP_EOL .
-		'			<dict>' . PHP_EOL .
-		'				<key>OverridePrimary</key>' . PHP_EOL .
-		'				<integer>1</integer>' . PHP_EOL .
-		'			</dict>' . PHP_EOL .
-		'			<key>PPP</key>' . PHP_EOL .
-		'			<dict>' . PHP_EOL .
-		'				<key>AuthName</key>' . PHP_EOL .
-		'				<string>' . $ecsid . '</string>' . PHP_EOL .
-		'				<key>CCPEnabled</key>' . PHP_EOL .
-		'				<integer>1</integer>' . PHP_EOL .
-		'				<key>CCPMPPE128Enabled</key>' . PHP_EOL .
-		'				<integer>1</integer>' . PHP_EOL .
-		'				<key>CCPMPPE40Enabled</key>' . PHP_EOL .
-		'				<integer>1</integer>' . PHP_EOL .
-		'				<key>CommRemoteAddress</key>' . PHP_EOL .
-		'				<string>pptp0.kuins.kyoto-u.ac.jp</string>' . PHP_EOL .
-		'			</dict>' . PHP_EOL .
-		'			<key>PayloadDescription</key>' . PHP_EOL .
-		'			<string>認証を含め、VPN 設定を構成します。</string>' . PHP_EOL .
-		'			<key>PayloadDisplayName</key>' . PHP_EOL .
-		'			<string>VPN（KUINS-PPTP）</string>' . PHP_EOL .
-		'			<key>PayloadIdentifier</key>' . PHP_EOL .
-		'			<string>com.ryotakameoka.kyotounivprofile.VPN</string>' . PHP_EOL .
-		'			<key>PayloadOrganization</key>' . PHP_EOL .
-		'			<string>Ryota Kameoka</string>' . PHP_EOL .
-		'			<key>PayloadType</key>' . PHP_EOL .
-		'			<string>com.apple.vpn.managed</string>' . PHP_EOL .
-		'			<key>PayloadUUID</key>' . PHP_EOL .
-		'			<string>5F4BC06D-3594-4DEA-9D60-B92978662CB0</string>' . PHP_EOL .
-		'			<key>PayloadVersion</key>' . PHP_EOL .
-		'			<integer>1</integer>' . PHP_EOL .
-		'			<key>Proxies</key>' . PHP_EOL .
-		'			<dict>' . PHP_EOL .
-		'				<key>ProxyAutoConfigEnable</key>' . PHP_EOL .
-		'				<integer>1</integer>' . PHP_EOL .
-		'				<key>ProxyAutoConfigURLString</key>' . PHP_EOL .
-		'				<string>http://wpad.kuins.net/proxy.pac</string>' . PHP_EOL .
-		'			</dict>' . PHP_EOL .
-		'			<key>UserDefinedName</key>' . PHP_EOL .
-		'			<string>KUINS-PPTP</string>' . PHP_EOL .
-		'			<key>VPNType</key>' . PHP_EOL .
-		'			<string>PPTP</string>' . PHP_EOL .
-		'		</dict>' . PHP_EOL .
-		'	</array>' . PHP_EOL .
-		'	<key>PayloadDescription</key>' . PHP_EOL .
-		'	<string>京都大学学生のための iPhone 構成プロファイルです。Wi-Fi (MIAKO), VPN (KUINS PPTP), 学生用メール (KUMOI) の設定を一括で行います。' . PHP_EOL .
-		'This is an iPhone configuration profile which enables students in Kyoto University to set up WiFi (MIAKO), VPN (KUINS PPTP), and e-mail service (KUMOI) simultaneously.</string>' . PHP_EOL .
-		'	<key>PayloadDisplayName</key>' . PHP_EOL .
-		'	<string>KyotoUnivProfile (Unofficial)</string>' . PHP_EOL .
-		'	<key>PayloadIdentifier</key>' . PHP_EOL .
-		'	<string>com.ryotakameoka.kyotounivprofile</string>' . PHP_EOL .
-		'	<key>PayloadOrganization</key>' . PHP_EOL .
-		'	<string>Ryota Kameoka</string>' . PHP_EOL .
-		'	<key>PayloadRemovalDisallowed</key>' . PHP_EOL .
-		'	<false/>' . PHP_EOL .
-		'	<key>PayloadType</key>' . PHP_EOL .
-		'	<string>Configuration</string>' . PHP_EOL .
-		'	<key>PayloadUUID</key>' . PHP_EOL .
-		'	<string>C3EA112F-3F7B-4C9C-9806-05C8109C8AD9</string>' . PHP_EOL .
-		'	<key>PayloadVersion</key>' . PHP_EOL .
-		'	<integer>1</integer>' . PHP_EOL .
-		'</dict>' . PHP_EOL .
-		'</plist>';
+$file = <<<EOT
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+	<key>PayloadContent</key>
+	<array>
+		<dict>
+			<key>AutoJoin</key>
+			<true/>
+			<key>HIDDEN_NETWORK</key>
+			<false/>
+			<key>PayloadDescription</key>
+			<string>ワイヤレス接続設定を構成します。</string>
+			<key>PayloadDisplayName</key>
+			<string>Wi-Fi（MIAKO）</string>
+			<key>PayloadIdentifier</key>
+			<string>me.ryota-ka.kyotounivprofile.</string>
+			<key>PayloadOrganization</key>
+			<string>Ryota Kameoka</string>
+			<key>PayloadType</key>
+			<string>com.apple.wifi.managed</string>
+			<key>PayloadUUID</key>
+			<string>81078CC2-BC4E-4D41-8EC7-D3028D34C000</string>
+			<key>PayloadVersion</key>
+			<integer>1</integer>
+			<key>ProxyType</key>
+			<string>None</string>
+			<key>SSID_STR</key>
+			<string>MIAKO</string>
+		</dict>
+		<dict>
+			<key>EmailAccountDescription</key>
+			<string>KUMOI</string>
+			<key>EmailAccountName</key>
+			<string>$name</string>
+			<key>EmailAccountType</key>
+			<string>EmailTypeIMAP</string>
+			<key>EmailAddress</key>
+			<string>$localpart@st.kyoto-u.ac.jp</string>
+			<key>IncomingMailServerAuthentication</key>
+			<string>EmailAuthPassword</string>
+			<key>IncomingMailServerHostName</key>
+			<string>outlook.office365.com</string>
+			<key>IncomingMailServerPortNumber</key>
+			<integer>993</integer>
+			<key>IncomingMailServerUseSSL</key>
+			<true/>
+			<key>IncomingMailServerUsername</key>
+			<string>$ecsid@st.kyoto-u.ac.jp</string>
+			<key>OutgoingMailServerAuthentication</key>
+			<string>EmailAuthPassword</string>
+			<key>OutgoingMailServerHostName</key>
+			<string>smtp.office365.com</string>
+			<key>OutgoingMailServerPortNumber</key>
+			<integer>587</integer>
+			<key>OutgoingMailServerUseSSL</key>
+			<true/>
+			<key>OutgoingMailServerUsername</key>
+			<string>$ecsid@st.kyoto-u.ac.jp</string>
+			<key>OutgoingPasswordSameAsIncomingPassword</key>
+			<true/>
+			<key>PayloadDescription</key>
+			<string>メールアカウントを構成します。</string>
+			<key>PayloadDisplayName</key>
+			<string>IMAP アカウント（KUMOI）</string>
+			<key>PayloadIdentifier</key>
+			<string>me.ryota-ka.kyotounivprofile.メール</string>
+			<key>PayloadOrganization</key>
+			<string>Ryota Kameoka</string>
+			<key>PayloadType</key>
+			<string>com.apple.mail.managed</string>
+			<key>PayloadUUID</key>
+			<string>C7998766-BDE1-4ACA-96B7-67D9FB750F2D</string>
+			<key>PayloadVersion</key>
+			<integer>1</integer>
+			<key>PreventAppSheet</key>
+			<false/>
+			<key>PreventMove</key>
+			<false/>
+			<key>SMIMEEnabled</key>
+			<false/>
+		</dict>
+		<dict>
+			<key>EAP</key>
+			<dict/>
+			<key>IPv4</key>
+			<dict>
+				<key>OverridePrimary</key>
+				<integer>1</integer>
+			</dict>
+			<key>PPP</key>
+			<dict>
+				<key>AuthName</key>
+				<string>$ecsid</string>
+				<key>CCPEnabled</key>
+				<integer>1</integer>
+				<key>CCPMPPE128Enabled</key>
+				<integer>1</integer>
+				<key>CCPMPPE40Enabled</key>
+				<integer>1</integer>
+				<key>CommRemoteAddress</key>
+				<string>pptp0.kuins.kyoto-u.ac.jp</string>
+			</dict>
+			<key>PayloadDescription</key>
+			<string>認証を含め、VPN 設定を構成します。</string>
+			<key>PayloadDisplayName</key>
+			<string>VPN（KUINS-PPTP）</string>
+			<key>PayloadIdentifier</key>
+			<string>me.ryota-ka.kyotounivprofile.VPN</string>
+			<key>PayloadOrganization</key>
+			<string>Ryota Kameoka</string>
+			<key>PayloadType</key>
+			<string>com.apple.vpn.managed</string>
+			<key>PayloadUUID</key>
+			<string>5F4BC06D-3594-4DEA-9D60-B92978662CB0</string>
+			<key>PayloadVersion</key>
+			<integer>1</integer>
+			<key>Proxies</key>
+			<dict>
+				<key>ProxyAutoConfigEnable</key>
+				<integer>1</integer>
+				<key>ProxyAutoConfigURLString</key>
+				<string>http://wpad.kuins.net/proxy.pac</string>
+			</dict>
+			<key>UserDefinedName</key>
+			<string>KUINS-PPTP</string>
+			<key>VPNType</key>
+			<string>PPTP</string>
+		</dict>
+	</array>
+	<key>PayloadDescription</key>
+	<string>京都大学学生のための iPhone 構成プロファイルです。Wi-Fi (MIAKO), VPN (KUINS-PPTP), 学生用メール (KUMOI) の設定を一括で行います。</string>
+	<key>PayloadDisplayName</key>
+	<string>KyotoUnivProfile</string>
+	<key>PayloadIdentifier</key>
+	<string>me.ryota-ka.kyotounivprofile</string>
+	<key>PayloadOrganization</key>
+	<string>Ryota Kameoka</string>
+	<key>PayloadRemovalDisallowed</key>
+	<false/>
+	<key>PayloadType</key>
+	<string>Configuration</string>
+	<key>PayloadUUID</key>
+	<string>C3EA112F-3F7B-4C9C-9806-05C8109C8AD9</string>
+	<key>PayloadVersion</key>
+	<integer>1</integer>
+</dict>
+</plist>
+EOT;
 
 header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
-header('Content-Disposition: attachment; filename="kyoto_univ_profile.mobileconfig"');
+header('Content-Disposition: attachment; filename="kyoto_univ_profile_' . $ecsid . '.mobileconfig"');
 echo $file;
