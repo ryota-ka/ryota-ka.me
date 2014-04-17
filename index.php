deea77149b121e3639230262ac62e82a79ba72ef
noise0607 <noise0607@gmail.com>
2014-04-17 15:09:50 +0900

    some updates

--- index.php
+++ index.php
@@ -18,34 +18,46 @@
 		</nav>
 		<div id="container">
 			<section id="logo">
-				<h1>Ryota-ka.me</h1>
+				<article>
+					<h1>Ryota-ka.me</h1>
+				</article>
 			</section>
-			<hr />
+
 			<section id="aboutme">
-				<h2>about me</h2>
-				<div>
-					<p class="name">Name : 亀岡 亮太 (Ryota Kameoka)</p>
-				</div>
-				<ul id="findmeontheweb">
-					<li id="link-facebook" data-href="http://www.facebook.com/kameoka.ryota"></li>
-					<li id="link-twitter" data-href="http://twitter.com/invendu"></li>
-					<li id="link-foursquare" data-href="http://ja.foursquare.com/user/4420704"></li>
-					<li id="link-lastfm" data-href="http://www.last.fm/user/ryotakameoka"></li>
-					<li id="link-youtube" data-href="http://www.youtube.com/channel/UC7pkiEXXwH1PiR-2xnHNLmQ"></li>
-					<li id="link-skype" data-href="skype:noise0607?userinfo"></li>
-				</ul>
+				<article>
+					<h2>about me</h2>
+					<div>
+						<p class="name">Name : 亀岡 亮太 (Ryota Kameoka)</p>
+					</div>
+					<div id="findmeontheweb">
+						<a id="link-facebook" href="http://www.facebook.com/kameoka.ryota"></a>
+						<a id="link-twitter" href="http://twitter.com/invendu"></a>
+						<a id="link-foursquare" href="http://ja.foursquare.com/user/4420704"></a>
+						<a id="link-lastfm" href="http://www.last.fm/user/ryotakameoka"></a>
+						<a id="link-youtube" href="http://www.youtube.com/channel/UC7pkiEXXwH1PiR-2xnHNLmQ"></a>
+						<a id="link-skype" href="skype:noise0607?userinfo"></a>
+					</div>
+				</article>
 			</section>
-			<hr />
+
 			<section id="works">
-
+				<article>
+					<ul>
+						<li><a href="./kyotounivprofile/">KyotoUnivProfile</a> 入力された情報に基づき、Wi-Fi (MIAKO), VPN (KUINS-PPTP), 学生用メール (KUMOI) の設定を一括で行う iPhone 構成プロファイルを作成します。</li>
+					</ul>
+				</article>
 			</section>
-			<hr />
-			<section>
 
+			<section>
+				<article>
+					<p>Phat Phunktion, the band apart, The Baker Brothers なんかが好き。</p>
+				</article>
 			</section>
-			<hr />
-			<section>
 
+			<section id="contact">
+				<article>
+					mail : noise0607@gmail.com
+				</article>
 			</section>
 		</div>
 	</body>
